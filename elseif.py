abdc7bbf661d8777291436714f8154ca6aa30a11
Brahm vijay <brahmvijay2002@gmail.com>
2022-08-17 14:59:56 +0530

    else if statement learned

---
+++ elseif.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a>=b and a>=c :
+    print(a,"is greater ")
+elif b>=a and b>=c : 
+    print(b,"is greater ")
+else :
+    print(c,"is greater ")
+#in python we dont write else if as "else if " but we use "elif" on that place 
