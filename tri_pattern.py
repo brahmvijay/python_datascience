09d5a3a7ee70615e785212e6360d33d4a5c2f4c1
Brahm vijay <brahmvijay2002@gmail.com>
2022-08-18 19:47:11 +0530

    this is the code for print alphabet order

--- tri_pattern.py
+++ tri_pattern.py
@@ -1,16 +1,21 @@
+from re import A
+
+
 a = int(input())
 i = 1
 while i<=a :
     k = 1
-    j = i
-    while k<=i :
-        print(j,end = " ")
-        j = j + 1
+    while k<=a :
+        l = 64 + k
+        print(chr(l),end = " ")
         k = k + 1
     print()
     i = i + 1 
+# to print alphabet in python we have 2 methods 
+#1.ord('A') --> it gives the ASCII value of the alphabet 
+#2.chr(65) --> it gives the alphabet of the corresponding number 
 #print this pattern 
-# 1 
-# 2 3
-# 3 4 5
-# 4 5 6 7
+# A B C D 
+# A B C D
+# A B C D
+# A B C D
