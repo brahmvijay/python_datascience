d57da62cc58087e1656a52ecc3134274102e6794
Brahm vijay <brahmvijay2002@gmail.com>
2022-08-18 19:21:44 +0530

    program for triangular patterns

---
+++ tri_pattern.py
@@ -0,0 +1,16 @@
+a = int(input())
+i = 1
+while i<=a :
+    k = 1
+    j = i
+    while k<=i :
+        print(j,end = " ")
+        j = j + 1
+        k = k + 1
+    print()
+    i = i + 1 
+#print this pattern 
+# 1 
+# 2 3
+# 3 4 5
+# 4 5 6 7
